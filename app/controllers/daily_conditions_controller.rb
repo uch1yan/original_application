2e54a80503c143495b6980ca09ecd4e4e2f6e6f0
manami young <manayou1125@outlook.jp>
2022-10-06 11:08:55 +0900

    daily_conditions/new画面の実装、モデルにenum記載

--- app/controllers/daily_conditions_controller.rb
+++ app/controllers/daily_conditions_controller.rb
@@ -1,10 +1,20 @@
 class DailyConditionsController < ApplicationController
   def new
+    @condition = DailyCondition.new
   end
 
   def edit
+    @condition = DailyCondition.find(params[:id])
   end
 
   def show
+    @condition = DailyCondition.find(params[:id])
   end
+
+  private
+    def daily_condition_params
+      params.require(:daily_condition).permit(:date, :mood, :temperature, :sleep, :comment)
+    end
+
+
 end
--- app/models/daily_condition.rb
+++ app/models/daily_condition.rb
@@ -1,2 +1,4 @@
 class DailyCondition < ApplicationRecord
+  enum mood: { 良い: 0, まあまあ: 1, 悪い: 2 }
+	enum toilet: { 快便: 0, 柔らかめ: 1, 固め: 2, コロコロ: 3, 下痢: 4 }
 end
