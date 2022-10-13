255f011df51e42201dd2e5f390d8ce5a9805992a
manami young <manayou1125@outlook.jp>
2022-10-13 17:25:02 +0900

    rails_admin, cancancanの導入

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,16 +1,16 @@
 RailsAdmin.config do |config|
-  config.asset_source = :webpacker
+  # config.asset_source = :webpacker
 
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
   ## == CancanCan ==
-  # config.authorize_with :cancancan
+  config.authorize_with :cancancan
 
   ## == Pundit ==
   # config.authorize_with :pundit
