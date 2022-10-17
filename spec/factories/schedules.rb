272f5cae30f7bb1e53381432b5754048d4ffced8
manami young <manayou1125@outlook.jp>
2022-10-17 11:45:24 +0900

    user_spec.rbの編集、他のrspecの内容の案出し

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -1,8 +1,16 @@
-FactoryBot.define do
-  factory :schedule do
-    title { "MyString" }
-    content { "MyText" }
-    date { "2022-10-07 09:29:33" }
-    kid { nil }
-  end
-end
+# FactoryBot.define do
+#   factory :schedule do
+#     title { "お遊戯会" }
+#     content { "保育園でお遊戯会" }
+#     date { "2022-10-01 12:00:00" }
+#     kid { id: 1 }
+#   end
+
+#   factory :second_schedule do
+#     title { "予防接種" }
+#     content { "水疱瘡の予防接種" }
+#     date { "2022-10-02 12:00:00" }
+#     kid { id: 1 }
+#   end
+
+# end
