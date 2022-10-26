1dfd5657aafd4216a08576478df1560927fc99ea
manami young <manayou1125@outlook.jp>
2022-10-26 12:29:47 +0900

    ユーザー情報編集後のエラーを解消

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: t('notice.create_posts') }
+        format.html { redirect_to posts_url, notice: t('notice.create_posts') }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,17 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
+  def admin_guest_sign_in
+    user = User.guest_admin
+    sign_in user
+    redirect_to root_path, notice: '管理者ゲストユーザーとしてログインしました。'
+  end
   # GET /resource/sign_in
   # def new
   #   super
