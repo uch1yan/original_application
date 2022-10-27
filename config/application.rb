ec771e66036e2de95ac81f4aa29ba2a77bfdbed9
manami young <manayou1125@outlook.jp>
2022-10-27 14:35:39 +0900

    招待メールのurl変更

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,5 @@ module OriginalApplication
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-
+  end
 end
