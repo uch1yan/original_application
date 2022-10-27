7bc2188b935cd8a2d3cb4670c6903f35d774c077
manami young <manayou1125@outlook.jp>
2022-10-27 14:09:12 +0900

    招待メールのurl変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :admin])																										
     devise_parameter_sanitizer.permit(:invite) { |u| u.permit(:email, :name, :family_id) }
     devise_parameter_sanitizer.permit(:accept_invitation) { |u| u.permit(:password, :password_confirmation, :invitation_token, :name, :family_id) }
-  end																										
+  end	
+  
+  def default_url_options
+    if Rails.env.production?
+      {:host => "hidden-scrubland-42949.herokuapp.com/"}
+    else  
+      {}
+    end
+  end
 end
