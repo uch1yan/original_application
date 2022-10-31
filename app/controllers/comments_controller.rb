90836d13338817a456574d738551bfb59931bb69
manami young <manayou1125@outlook.jp>
2022-10-31 15:13:23 +0900

    デザインの微調整

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_condition, only: %i[ create edit update ]
+  
   def create
     @condition = DailyCondition.find(params[:daily_condition_id])
     @comment = @condition.comments.build(comment_params)
@@ -15,16 +16,16 @@ class CommentsController < ApplicationController
     end
   end
 
-  def create
-    @comment = @condition.comments.build(comment_params)
-    respond_to do |format|
-      if @comment.save
-        format.js { render :index }
-      else
-        format.html { redirect_to daily_condition_path(@condition), notice: '投稿できませんでした...' }
-      end
-    end
-  end
+  # def create
+  #   @comment = @condition.comments.build(comment_params)
+  #   respond_to do |format|
+  #     if @comment.save
+  #       format.js { render :index }
+  #     else
+  #       format.html { redirect_to daily_condition_path(@condition), notice: '投稿できませんでした...' }
+  #     end
+  #   end
+  # end
 
   def edit
     @comment = @condition.comments.find(params[:id])
